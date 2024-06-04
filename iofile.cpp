04cae79d0f97f5dd0996839ae50fd5edc3d38232
aryadamara <aryadamara@DESKTOP-PT9UTUQ>
2024-06-04 14:41:23 +0700

    int main() {     string baris;
        // membuka file dalam mode menulis.
        ofstream outfile;
        // menunjuk ke sebuah nama file
        outfile.open("contohfiletxt");

---
+++ iofile.cpp
@@ -0,0 +1,29 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main() {
+    string baris;
+
+    // membuka file dalam mode menulis.
+    ofstream outfile;
+    // menunjuk ke sebuah nama file
+    outfile.open("contohfiletxt");
+
+    cout << ">= Menulis file, \'q\ untuk keluar" << endl;
+
+    // unlimited loop untuk menulis 
+    while (true) {
+        cout << "-";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;    
+        // menulis dan memasukkan nilai dari 'baris'ke dalam file        
+        outfile << baris << endl;
+        }
+        //selesai dalam menulis sekarang tutup file nya
+}
