01c807be3f8831401adf7b11aa4d28a085f30c32
aryadamara <aryadamara@DESKTOP-PT9UTUQ>
2024-06-04 14:51:47 +0700

    //membuka file dalam mode membaca         ifstream infile;         //menunjuk ke sebuah file         infile.open("contohfile.txt");

--- iofile.cpp
+++ iofile.cpp
@@ -26,4 +26,25 @@ int main() {
         outfile << baris << endl;
         }
         //selesai dalam menulis sekarang tutup file nya
+        outfile.close();
+
+        //membuka file dalam mode membaca
+        ifstream infile;
+        //menunjuk ke sebuah file
+        infile.open("contohfile.txt");
+
+        cout << endl << ">= membuka dan membaca file " << endl;
+        //jika file ada maka
+        if (infile.is_open()) {
+            //melakukan perulangan setiap baris
+            while (getline(infile, baris)) {
+                //dan tampilkan disini
+                cout << baris << '\n';
+            }
+            //tutup file tersebut setelah selesai
+            infile.close();
+        }
+        //jika tidak ditemukan file maka akan menampilkan ini
+        else cout << "unable to open file";
+        return 0;
 }
